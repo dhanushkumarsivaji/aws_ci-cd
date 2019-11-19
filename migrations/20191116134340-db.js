3b228889cd2df3032ce294cf2df30bab18c63248
DhanushkumarSivaji <dhanushkumarstudy@gmail.com>
2019-11-19 11:32:51 +0530

    v10

--- migrations/20191116134340-db.js
+++ migrations/20191116134340-db.js
@@ -1,7 +1,7 @@
 module.exports = {
   async up(db) {
     
-    await db.collection('quotes').updateOne({ quotes: "work hard" }, { $set:{ quotes:"checking log files"} });
+    await db.collection('quotes').updateOne({ quotes: "work hard" }, { $set:{ quotes:"You are born to win"} });
   },
 
   async down(db) {
