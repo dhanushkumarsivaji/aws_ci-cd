39d96e7a42d3b0098579f6d39be9ca0e183abf36
DhanushkumarSivaji <dhanushkumarstudy@gmail.com>
2019-11-20 17:19:35 +0530

    Migration Script v1

--- migrations/20191116134340-db.js
+++ migrations/20191116134340-db.js
@@ -1,11 +1,11 @@
 module.exports = {
   async up(db) {
     
-    await db.collection('quotes').updateOne({ quotes: "You are born to win" }, { $set:{ quotes:"Life is not an easy task"} });
+    await db.collection('quotes').updateOne({ quotes: "work hard" }, { $set:{ quotes:"Life is not an easy task"} });
   },
 
   async down(db) {
 
-    await db.collection('quotes').updateOne({ quotes: "checking log files" }, { $set:{ quotes:"work hard"} });
+    await db.collection('quotes').updateOne({ quotes: "You are born to win" }, { $set:{ quotes:"work hard"} });
   }
 };
