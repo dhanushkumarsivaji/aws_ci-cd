70fe76930b7056d7f8cd43896dee399ccefcab97
DhanushkumarSivaji <dhanushkumarstudy@gmail.com>
2019-11-20 14:02:15 +0530

    integreation testing v1

--- tests/unit/intergeration.test.js
+++ tests/unit/intergeration.test.js
@@ -1,9 +1,10 @@
 const request = require('supertest')
 const app = require('../../server');
-
+const mongoose = require('mongoose')
 test("Should signin a user",async()=>{
     await request(app).post('/api/auth').send({
         email:"dhanushkumarstudy@gmail.com",
 	    password:"123456"
-    }).expect(200)
+    }).expect(300)
+
 })
