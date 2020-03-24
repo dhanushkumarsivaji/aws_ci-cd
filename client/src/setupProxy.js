ec9d864711053570f80a12cabcff4a1e3d9b9c8d
Dhanush kumar Sivaji <36889650+DhanushkumarSivaji@users.noreply.github.com>
2020-03-24 08:38:45 +0530

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     proxy({
-      target: 'http://localhost:8081',
+      target: 'http://localhost:8000',
       changeOrigin: true,
     })
   );
