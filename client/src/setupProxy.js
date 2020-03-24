53332ab8d4a808032b9457c12f25760dfff452c9
Dhanush kumar Sivaji <36889650+DhanushkumarSivaji@users.noreply.github.com>
2020-03-24 09:21:49 +0530

    test

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/apis',
     createProxyMiddleware({
       target: 'http://localhost:8000',
       changeOrigin: true,
